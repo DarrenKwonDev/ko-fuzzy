e6e41ce72cab5aa3e2f004a18bdbd2d1fab29426
DarrenKwonDev <anatomy1545@gmail.com>
2021-06-01 03:50:08 +0900

    readme

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # autoFuzzy
 
+_on developing..._
+
 ## how to use
 
 ```typescript
-const autoFuze = new AutoFuzzy(Object, options)
 ...
 ```
 
@@ -14,12 +15,22 @@ console.log(exactKoMatch("서우").test("서울특별시")); // true.
 console.log(/"서우"/.test("서울특별시")); // false.
 ```
 
+```typescript
+// 그 외의 util 함수들
+getUnicode("가"); // U+ac00
+
+extractKoPhonemes("안");
+// {initial: 'ㅇ', medial: 'ㅏ', finale: 'ㄴ', initialOffset: 11, medialOffset: 0, finaleOffset: 4 }
+```
+
 ## 유니코드 및 관련 작업 설명글
 
 [유니코드와 한글의 영역](https://darrengwon.tistory.com/1425)  
 [초중종성 분리작업](https://darrengwon.tistory.com/1426)
 
 ### 한글 관련 용어 정리
+
+음소(Phonemes)
 
 초성(初聲, onset)  
 중성(中聲, nucleus)  
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,45 @@
 import fs from "fs";
 import path from "path";
 
-import extractKoChar from "./tools/extractKoPhonemes";
+import extractKoPhonemes from "./tools/extractKoPhonemes";
 import fuzzyEnMatch from "./core/fuzzyEnMatch";
 import getUnicode from "./tools/getUnicode";
 import exactKoMatch from "./core/exactKoMatch";
 
-console.log(exactKoMatch("서우").test("서울특별시")); // true.
-console.log(/"서우"/.test("서울특별시")); // false.
+fs.readFile(path.join(__dirname, "../example/data/city.json"), "utf8", (err, data) => {
+  if (err) throw err;
 
-// const fuzzyEnregex = fuzzyEnMatch("ct");
-// console.log(fuzzyEnregex); // /c.*?t/
+  let city = JSON.parse(data);
+  city
+    .filter((elem: any) => {
+      return exactKoMatch("붕").test(elem["행정구역"]);
+    })
+    .map((elem: any) => console.log(elem["행정구역"]));
+});
+
+// const a = "서울특별시".search(exactMatch("서우")) !== -1;
+// console.log(a); // true
+
+// const regex = createFuzzyMatcher("ct");
+
+// console.log(regex);
+
+// console.log([..."hello"]);
+
+// console.log(exactMatch("블"));
+// console.log(exactMatch("블래"));
+// console.log(exactMatch("블랙"));
+
+// const movieRegex = exactMatch("블ㄹ");
+
+// console.log(exactMatch("블"));
+
+// console.log(extractKoChar("ㅇ"));
+console.log(extractKoPhonemes("안"));
+// console.log(extractKoChar("훟"));
+// console.log(extractKoChar("밝"));
 
 // console.log(getUnicode("߸"));
-// console.log(getUnicode("한"))
-// console.log(getUnicode("가"))
+// console.log(getUnicode("한"));
+// console.log(getUnicode("가"));
 // console.log(extractKoChar("한"))
