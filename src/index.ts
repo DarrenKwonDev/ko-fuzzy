b27f858dcfc117bc47dfaa19de33a607d482a5a1
DarrenKwonDev <anatomy1545@gmail.com>
2021-06-03 11:23:53 +0900

    install husky

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,10 @@ import regexMatch from "./core/regexMatch";
 //     .map((elem: any) => console.log(elem["행정구역"]));
 // });
 
-// console.log(regexMatch("가누", { consonantMatch: false }));
-// console.log(regexMatch("후ㄱ도", { consonantMatch: true }));
-// console.log(regexMatch("후ㄱ도", { consonantMatch: true, fuzzy: true }));
-// console.log(regexMatch("ㅂㅇ", { consonantMatch: false, fuzzy: true }));
-// console.log(regexMatch("ㅂㅇbdㅇ", { consonantMatch: true, fuzzy: true }));
-// console.log(regexMatch("ㅂㅇbd", { consonantMatch: true, fuzzy: true }));
+console.log(regexMatch("가누", { consonantMatch: false }));
+console.log(regexMatch("후ㄱ도", { consonantMatch: true }));
+console.log(regexMatch("후ㄱ도", { consonantMatch: true, fuzzy: true }));
+console.log(regexMatch("ㅂㅇ", { consonantMatch: false, fuzzy: true }));
+console.log(regexMatch("ㅂㅇbdㅇ", { consonantMatch: true, fuzzy: true }));
 // console.log(regexMatch("ㅁㄴㅍ루", { consonantMatch: true })); // /[마-밓][나-닣][파-핗][루-륗]/
 // console.log(regexMatch("as안v", { consonantMatch: true })); // /as안v/
--- src/tools/getUnicode.ts
+++ src/tools/getUnicode.ts
@@ -1,9 +1,9 @@
-function getUnicode(word:string) {
-    let base = word.charCodeAt(0).toString(16)
-    if (base.length < 4) {
-        base = base.padStart(4, '0')
-    }
-    return "U+" + base
+function getUnicode(word: string) {
+  let base = word.charCodeAt(0).toString(16);
+  if (base.length < 4) {
+    base = base.padStart(4, "0");
+  }
+  return "U+" + base;
 }
 
-export default getUnicode
+export default getUnicode;
